f63d8af6a6b6086e3c6562b6307d070d0ab9dbe4
clrnd <welcometothechango@gmail.com>
2023-10-24 15:35:47 -0300

    logging

--- run.py
+++ run.py
@@ -21,6 +21,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+logging.getLogger('httpx').setLevel(logging.WARNING)
 
 
 @aiocron.crontab('0 14 * * *', start=False)
