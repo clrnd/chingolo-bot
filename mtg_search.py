21cbc866a223224276d81c6c0c930c2329c367b2
clrnd <welcometothechango@gmail.com>
2023-10-24 15:33:44 -0300

    flatten list

--- mtg_search.py
+++ mtg_search.py
@@ -13,18 +13,26 @@ async def inline_query_handler(update, context):
 
     try:
         search = scrython.cards.Search(q=query)
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.5)
     except Exception as e:
         logging.error(e)
         return
 
     results = [
-        InlineQueryResultPhoto(
-            id=result['id'],
-            photo_url=result['image_uris']['normal'],
-            thumbnail_url=result['image_uris']['small'],
-        )
+        [
+            InlineQueryResultPhoto(
+                id=result['illustration_id'],
+                photo_url=result['image_uris']['normal'],
+                thumbnail_url=result['image_uris']['small'],
+            )
+        ] if 'image_uris' in result else [
+            InlineQueryResultPhoto(
+                id=face['illustration_id'],
+                photo_url=face['image_uris']['normal'],
+                thumbnail_url=face['image_uris']['small'],
+            ) for face in result['card_faces']
+        ]
         for result in search.data()[:10]
     ]
 
-    await update.inline_query.answer(results)
+    await update.inline_query.answer([item for items in results for item in items])
